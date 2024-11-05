cbc503750ddb4999a9922350c211a01e2bed321d
JulianeMaran32 <julianemaran@gmail.com>
2024-11-05 15:39:47 -0300

    feat: calendario

--- petshop-app/src/app/features/calendar/calendar.module.ts
+++ petshop-app/src/app/features/calendar/calendar.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PetshopCalendarComponent } from './petshop-calendar/petshop-calendar.component';
-import { EventDialogComponent } from './event-dialog/event-dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
-    FullCalendarModule,
-    PetshopCalendarComponent,
-    EventDialogComponent
+    FormsModule,
+    RouterModule,
+    FullCalendarModule
   ]
 })
 export class CalendarModule { }
--- petshop-app/src/app/features/calendar/calendar/calendar.component.ts
+++ petshop-app/src/app/features/calendar/calendar/calendar.component.ts
@@ -1,101 +1,92 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CalendarModule } from 'primeng/calendar';
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, EventSourceInput } from '@fullcalendar/core';
-import { PanelModule } from 'primeng/panel';
-import { EventDialogComponent } from '../event-dialog/event-dialog.component';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { LoggerService } from '../../../core/services/logger.service';
+import { AppointmentService } from '../../../core/services/appointment.service';
+import { AppointmentResponse } from '../../../shared/models/appointment/appointment-response.model';
 
 @Component({
-  selector: 'app-petshop-calendar',
+  selector: 'app-calendar',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule,
-    CalendarModule,
-    FullCalendarModule,
-    PanelModule,
-    EventDialogComponent,
-    DialogModule,
-    ButtonModule
+    FormsModule,
+    RouterModule,
+    FullCalendarModule
   ],
-  templateUrl: './petshop-calendar.component.html',
-  styleUrls: ['./petshop-calendar.component.scss']
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss'],
+  providers: [LoggerService, AppointmentService]
 })
-export class PetshopCalendarComponent implements OnInit {
-  
-  @ViewChild('fullCalendar') fullCalendar!: FullCalendarComponent;
-  @ViewChild(EventDialogComponent) eventDialog!: EventDialogComponent;
+export class CalendarComponent implements OnInit, AfterViewInit {
 
-  displayDialog: boolean = false; // Propriedade para exibir ou ocultar o modal
-  newEvent: any = {}; // Propriedade para armazenar o novo evento
-  selectedEvent: any = {}; // Propriedade para armazenar o evento selecionado
+  @ViewChild('fullCalendar') fullCalendar!: FullCalendarComponent;
+
+  displayDialog: boolean = false;
+  newEvent: any = {};
+  selectedEvent: any = {};
   eventSources: EventSourceInput[] = [];
   calendarOptions!: CalendarOptions;
 
-  constructor() { }
+  constructor(
+    private logger: LoggerService,
+    private appointmentService: AppointmentService
+  ) {
+    this.logger.info('CalendarComponent instanciado');
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.logger.debug('ngOnInit chamado');
     this.calendarOptions = {
       plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
       initialView: 'timeGridWeek',
-      locale: 'pt-br',
       timeZone: 'America/Sao_Paulo',
-      slotLabelInterval: '01:00:00', // Frequência dos rótulos dos intervalos de tempo (1 hora)
-      slotDuration: '00:30:00', // Duração de cada intervalo de tempo (30 minutos)
-      slotMinTime: '08:00:00', // Horário mínimo
-      slotMaxTime: '20:00:00', // Horário máximo
+      locale: 'pt-br',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      },
+      views: {
+        dayGridPlugin: {
+          titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' },
+          dayHeaderFormat: { weekday: 'short', month: 'numeric', day: 'numeric' },
+        },
+        list: {
+          titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' },
+          dayHeaderFormat: { weekday: 'short', month: 'numeric', day: 'numeric' },
+        }
+      },
+      slotLabelInterval: '01:00:00',
+      slotDuration: '00:30:00',
+      slotMinTime: '08:00:00',
+      slotMaxTime: '18:00:00',
       businessHours: [
         {
-          daysOfWeek: [1, 2, 3, 4, 5], // Segunda a Sexta
-          startTime: '09:00',
-          endTime: '12:00' // Antes do almoço
+          daysOfWeek: [1, 2, 3, 4, 5],
+          startTime: '08:00',
+          endTime: '18:00'
         },
         {
-          daysOfWeek: [1, 2, 3, 4, 5], // Segunda a Sexta
-          startTime: '13:00',
-          endTime: '18:00' // Após o almoço
-        },
-        {
-          daysOfWeek: [6], // Sábado
-          startTime: '09:00',
-          endTime: '12:00' // Antes do almoço
-        },
-        {
-          daysOfWeek: [6], // Sábado
-          startTime: '13:00',
-          endTime: '15:00' // Após o almoço
+          daysOfWeek: [6],
+          startTime: '08:00',
+          endTime: '14:00'
         }
       ],
       eventDurationEditable: false,
       eventResizableFromStart: false,
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,addEventButton,blockAgendaButton',
-      },
-      customButtons: {
-        addEventButton: {
-          text: 'Agendar',
-          click: this.showAddEventDialog.bind(this)
-        },
-        blockAgendaButton: {
-          text: 'Bloquear Agenda',
-          click: this.blockAgenda.bind(this)
-        }
-      },
       buttonText: {
         today: 'Hoje',
         month: 'Mês',
         week: 'Semana',
-        day: 'Dia',
+        day: 'Dia'
       },
       weekends: true,
       editable: true,
@@ -111,46 +102,54 @@ export class PetshopCalendarComponent implements OnInit {
       eventClassNames: this.customEventClassNames.bind(this),
       eventContent: this.customEventContent.bind(this),
       eventDidMount: this.customEventDidMount.bind(this),
-      viewDidMount: this.viewDidMount.bind(this) 
+      viewDidMount: this.viewDidMount.bind(this)
     };
 
-    // Adicionando uma fonte de eventos inicial
-    this.eventSources.push({
-      events: [
-        { id: '7', title: 'Rian | Consulta', start: '2024-10-28T10:30:00', end: '2024-10-28T11:00:00', color: 'blue' },
-        { id: '8', title: 'Suzi | Vacina', start: '2024-10-28T11:00:00', end: '2024-10-28T11:30:00', color: 'yellow' }
-      ]
-    });
+    this.loadAppointments();
   }
 
   ngAfterViewInit() {
+    this.logger.debug('ngAfterViewInit chamado');
     this.updateEventSources();
   }
 
+  loadAppointments(): void {
+    this.appointmentService.getAppointments().subscribe((appointments: AppointmentResponse[]) => {
+      const events = appointments.map(appointment => ({
+        id: appointment.id.toString(), // Convertendo id para string
+        title: `Pet ID: ${appointment.pet_id} | Veterinarian: ${appointment.veterinarian}`,
+        start: `${appointment.date}T${appointment.time}`,
+        end: `${appointment.date}T${appointment.time}`,
+        description: appointment.description
+      }));
+      this.eventSources.push({ events });
+      this.updateEventSources();
+    });
+  }
+
   handleSelect(selectInfo: any) {
-    // Lógica para manipular a seleção de intervalo de tempo
+    this.logger.info('Intervalo selecionado', selectInfo);
     alert(`Intervalo selecionado: ${selectInfo.startStr} até ${selectInfo.endStr}`);
   }
 
   handleDateClick(dateClickInfo: any) {
-    // Lógica para manipular o clique em intervalo de tempo
+    this.logger.info('Intervalo clicado', dateClickInfo);
     alert(`Intervalo clicado: ${dateClickInfo.dateStr}`);
   }
 
   handleEventClick(info: any) {
-    // Lógica para manipular o clique em evento
-    this.selectedEvent = { 
-      ...info.event.extendedProps, 
-      start: info.event.start, 
-      end: info.event.end, 
-      title: info.event.title 
+    this.logger.info('Evento clicado', info);
+    this.selectedEvent = {
+      ...info.event.extendedProps,
+      start: info.event.start,
+      end: info.event.end,
+      title: info.event.title
     };
-    this.eventDialog.mode = 'edit';
     this.displayDialog = true;
   }
 
   customEventClassNames(arg: any) {
-    // Adiciona classes CSS personalizadas aos eventos com base no tipo de evento
+    this.logger.debug('Adicionando classes CSS personalizadas aos eventos', arg);
     switch (arg.event.extendedProps.type) {
       case 'banho-tosa':
         return ['event-banho-tosa'];
@@ -170,25 +169,25 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   viewDidMount() {
-    // Adiciona a classe custom-button aos botões personalizados
+    this.logger.debug('viewDidMount chamado');
     const addEventButton = document.querySelector('.fc-addEventButton-button');
     if (addEventButton) {
       addEventButton.classList.add('custom-button');
     }
-  
+
     const blockAgendaButton = document.querySelector('.fc-blockAgendaButton-button');
     if (blockAgendaButton) {
       blockAgendaButton.classList.add('custom-button');
     }
   }
-  
+
   customEventContent(arg: any) {
-    // Personaliza o conteúdo HTML dos eventos
+    this.logger.debug('Personalizando o conteúdo HTML dos eventos', arg);
     return { html: `<b>${arg.event.title}</b><br><i>${arg.event.start.toLocaleTimeString()}</i>` };
   }
 
   customEventDidMount(arg: any) {
-    // Aplica lógica adicional ou estilos após a renderização dos eventos
+    this.logger.debug('Aplicando lógica adicional ou estilos após a renderização dos eventos', arg);
     if (arg.event.extendedProps.type === 'block') {
       arg.el.style.backgroundColor = 'red';
       arg.el.style.borderColor = 'red';
@@ -196,11 +195,12 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   addEvent() {
-    // Lógica para adicionar um evento
+    this.logger.info('Botão Agendar clicado');
     alert('Botão Agendar clicado!');
   }
 
   showAddEventDialog() {
+    this.logger.info('Exibindo diálogo para adicionar evento');
     this.newEvent = {
       title: '',
       start: '',
@@ -210,24 +210,21 @@ export class PetshopCalendarComponent implements OnInit {
         pet: ''
       }
     };
-    this.eventDialog.mode = 'add';
     this.displayDialog = true;
   }
 
   saveNewEvent(event: any) {
-    if (this.eventDialog.mode === 'add') {
-      this.addEventSource([event]);
-    } else {
-      this.updateEvent(event);
-    }
+    this.logger.info('Salvando novo evento', event);
     this.displayDialog = false;
   }
 
   cancelNewEvent() {
+    this.logger.info('Cancelando novo evento');
     this.displayDialog = false;
   }
 
   deleteEvent() {
+    this.logger.info('Deletando evento', this.selectedEvent);
     const calendarApi = this.fullCalendar.getApi();
     const calendarEvent = calendarApi.getEventById(this.selectedEvent.id);
     if (calendarEvent) {
@@ -237,7 +234,7 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   blockAgenda() {
-    // Lógica para bloquear a agenda
+    this.logger.info('Bloqueando agenda');
     const start = prompt('Digite a data e hora de início do bloqueio (YYYY-MM-DDTHH:MM:SS)');
     const end = prompt('Digite a data e hora de término do bloqueio (YYYY-MM-DDTHH:MM:SS)');
     if (start && end) {
@@ -248,22 +245,26 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   addEventSource(events: any[]) {
+    this.logger.info('Adicionando fonte de eventos', events);
     this.eventSources.push({ events });
     this.updateEventSources();
   }
 
   removeEventSource(index: number) {
+    this.logger.info('Removendo fonte de eventos', index);
     this.eventSources.splice(index, 1);
     this.updateEventSources();
   }
 
   updateEventSources() {
+    this.logger.debug('Atualizando fontes de eventos');
     const calendarApi = this.fullCalendar.getApi();
     calendarApi.removeAllEventSources();
     this.eventSources.forEach(source => calendarApi.addEventSource(source));
   }
 
   updateEvent(event: any) {
+    this.logger.info('Atualizando evento', event);
     const calendarApi = this.fullCalendar.getApi();
     const calendarEvent = calendarApi.getEventById(event.id);
     if (calendarEvent) {
@@ -284,7 +285,7 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   saveEvent(event: any) {
-    // Lógica para salvar o evento
+    this.logger.info('Salvando evento', event);
     const calendarApi = this.fullCalendar.getApi();
     const calendarEvent = calendarApi.getEventById(event.id);
     if (calendarEvent) {
@@ -297,7 +298,7 @@ export class PetshopCalendarComponent implements OnInit {
   }
 
   cancelEvent(event: any) {
-    // Lógica para cancelar o evento
+    this.logger.info('Cancelando evento', event);
     const calendarApi = this.fullCalendar.getApi();
     const calendarEvent = calendarApi.getEventById(event.id);
     if (calendarEvent) {
@@ -306,33 +307,4 @@ export class PetshopCalendarComponent implements OnInit {
     this.displayDialog = false;
   }
 
-  /**
-   * 1. Adicionar um novo evento
-   * 2. Atualizar um evento
-   * 3. Excluir um evento
-   * 4. Duração de um evento = 30min
-   * 5. Horários: 09:00 às 18:00 (Segunda a Sexta) e 09:00 às 14:00 (Sábado)
-   * 6. Eventos: Banho e Tosa, Banho, Tosa, Consulta, Vacina
-   * 7. Cores: Banho e Tosa (Verde), Banho (Verde Claro), Tosa (Laranja), Consulta (Azul), Vacina (Amarelo)
-   * 8. Exibir a hora no formato 24h
-   * 9. Exibir data e hora no formato dd/MM/yyyy HH:mm (Ex: 28/10/2024 10:00)
-   * 10. Exibir o nome do evento no tooltip
-   * 11. Exibir o nome do evento no título da página
-   * 12. Dias da semana em Português: Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado
-   * 13. Meses em Português: Janeiro, Fevereiro, Março, Abril, Maio, Junho, Julho, Agosto, Setembro, Outubro, Novembro, Dezembro
-   * 14. Adicionar um botão para adicionar um novo evento
-   * 15. Para o formulário de agendamento (adicionar / editar) abrir um modal/dialog
-   * 16. Exibir um campo para selecionar o tipo de evento (Banho e Tosa, Banho, Tosa, Consulta, Vacina), data e hora de início e fim
-   * 17. Exibir um campo para selecionar o pet (Nome, Raça, Idade, Peso, Porte)
-   * 18. Exibir um campo para selecionar o cliente (Nome, CPF, Telefone, E-mail)
-   * 19. Exibir um campo para selecionar o serviço (Nome, Preço, Duração)
-   * 20. Exibir um campo para selecionar o profissional (Nome, CPF, Telefone, E-mail)
-   * 21. Exibir um campo para selecionar o status do evento (Agendado, Em andamento, Concluído, Cancelado)
-   * 22. Exibir um campo para adicionar observações
-   * 23. Exibir um campo para adicionar anexos (Fotos, Vídeos, Documentos)
-   * 24. Exibir um campo para adicionar lembretes (E-mail, SMS, WhatsApp)
-   * 25. Exibir um campo para adicionar tags (Cores, Etiquetas)
-   * 26. Exibir um campo para adicionar histórico (Data, Hora, Evento, Observações)
-   */
-  
 }
