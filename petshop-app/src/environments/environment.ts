bdd6a8496d2df44d75649f7fa45dc294faf34874
JulianeMaran32 <julianemaran@gmail.com>
2024-11-05 15:49:16 -0300

    fix: altera docker, config json e ambiente

--- petshop-app/src/environments/environment.ts
+++ petshop-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'petshop-web-api-a5aeanc2g5gzgzer.eastus2-01.azurewebsites.net'
+    apiUrl: 'https://petshop-web-api-a5aeanc2g5gzgzer.eastus2-01.azurewebsites.net/',
     // apiUrl: 'http://localhost:8080'
 };
